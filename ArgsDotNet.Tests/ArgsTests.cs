96c3ca72c888c958338c2efbb700c4e4c61416ff
Jeka Vasiliev <zheka_v@inbox.ru>
2018-04-30 12:13:42 +0300

    More tests from book.

--- ArgsDotNet.Tests/ArgsTests.cs
+++ ArgsDotNet.Tests/ArgsTests.cs
@@ -26,7 +26,61 @@ namespace ArgsDotNet.Tests
         [Fact]
         public void TestWithNoSchemaButWithMultipleArguments()
         {
+            Action act = () => { new Args("", new[] { "-x", "-y" }); };
+            var exception = act.Should().Throw<ArgsException>().And;
+            exception.Code.Should().Be(ErrorCode.UNEXPECTED_ARGUMENT);
+            exception.ErrorArgumentId.Should().Be('x');
+        }
 
+        [Fact]
+        public void TestNonLetterSchema()
+        {
+            Action act = () => { new Args("*", new string[0]); };
+            act.Should().Throw<ArgsException>().And.ErrorArgumentId.Should().Be('*');
+        }
+
+        [Fact]
+        public void TestInvalidArgumentFormat()
+        {
+            Action act = () => { new Args("f~", new string[0]); };
+            var exception = act.Should().Throw<ArgsException>().And;
+            exception.Code.Should().Be(ErrorCode.INVALID_ARGUMENT_FORMAT);
+            exception.ErrorArgumentId.Should().Be('f');
+        }
+
+        [Fact]
+        public void TestSimpleBooleanPresent()
+        {
+            var args = new Args("x", new[] { "-x" });
+            args.Cardinality.Should().Be(1);
+            args.GetBoolean('x').Should().BeTrue();
+        }
+
+        [Fact]
+        public void TestSimpleStringPresent()
+        {
+            var args = new Args("x*", new[] { "-x", "param" });
+            args.Cardinality.Should().Be(1);
+            args.Has('x').Should().BeTrue();
+            args.GetString('x').Should().Be("param");
+        }
+
+        [Fact]
+        public void TestMissingStringArgument()
+        {
+            Action act = () => { new Args("x*", new[] { "-x" }); };
+            var exception = act.Should().Throw<ArgsException>().And;
+            exception.Code.Should().Be(ErrorCode.MISSING_STRING);
+            exception.ErrorArgumentId.Should().Be('x');
+        }
+
+        [Fact]
+        public void TestSpacesinFormat()
+        {
+            var args = new Args("x, y", new[] { "-xy" });
+            args.Cardinality.Should().Be(2);
+            args.GetBoolean('x').Should().BeTrue();
+            args.GetBoolean('y').Should().BeTrue();
         }
 
         [Fact]
